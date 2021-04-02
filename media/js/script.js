c4c4ed760485049011ec5a157963e2b9d0e68aee
Tim Jones <t27.jones@connect.qut.edu.au>
2021-04-02 14:39:17 +1000

    Added responsive elements and styles

--- media/js/script.js
+++ media/js/script.js
@@ -2,6 +2,39 @@ function setCopyrightYear(){
 	var theDate = new Date(); 
 	$(".year").text(theDate.getFullYear());
 }
+function navBGScroll(){
+	//Check on the navbar on start
+	var scrollTop = $(document).scrollTop(),
+		navElement = $('.nav-container');
+	if(scrollTop > 5){
+		navElement.addClass('solid');
+	}
+	else {
+		navElement.removeClass('solid');
+	}
+	$(window).scroll(function(){
+		var scrollTop = $(document).scrollTop();
+		if(scrollTop > 5){
+			navElement.addClass('solid');
+		}
+		else {
+			navElement.removeClass('solid');
+		}
+	});
+}
+// function fadeNavBrandOnMenuOpen(){
+// 	$('.navbar-toggler').click(function(){
+// 		var isOpen = $(this).attr('aria-expanded');
+// 		if(isOpen){
+// 			$('.navbar-brand').addClass('fade-out');
+// 		}
+// 		else {
+// 			$('.navbar-brand').removeClass('fade-out');
+// 		}
+// 	});
+// }
 $(document).ready(function(){
 	setCopyrightYear();
+	navBGScroll();
+	// fadeNavBrandOnMenuOpen();
 });
