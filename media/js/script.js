4590936bab67f72e1edf422f35c854f72ce376cc
Tim Jones <t27.jones@connect.qut.edu.au>
2021-04-04 16:47:49 +1000

    Added proper links to nav and footer

--- media/js/script.js
+++ media/js/script.js
@@ -3,7 +3,6 @@ function setCopyrightYear(){
 	$(".year").text(theDate.getFullYear());
 }
 function navBGScroll(){
-	//Check on the navbar on start
 	var scrollTop = $(document).scrollTop(),
 		navElement = $('.nav-container');
 	if(scrollTop > 5){
@@ -12,45 +11,14 @@ function navBGScroll(){
 	else {
 		navElement.removeClass('solid');
 	}
-	$(window).scroll(function(){
-		var scrollTop = $(document).scrollTop();
-		if(scrollTop > 5){
-			navElement.addClass('solid');
-		}
-		else {
-			navElement.removeClass('solid');
-		}
-	});
 }
-// function fadeNavBrandOnMenuOpen(){
-// 	$('.navbar-toggler').click(function(){
-// 		var isOpen = $(this).attr('aria-expanded');
-// 		if(isOpen){
-// 			$('.navbar-brand').addClass('fade-out');
-// 		}
-// 		else {
-// 			$('.navbar-brand').removeClass('fade-out');
-// 		}
-// 	});
-// }
-
-
 function createScrollReveal(){
 	// Reveals the scroll-prompt when the users 
 	// scrolls it into view on the Learn page
 	ScrollReveal({ duration: 1000 });
 	ScrollReveal().reveal('.scroll-prompt', { delay: 1000 });
 }
-// Change button text on Trade page 
-// from "Read the Whitepaper" to
-// "Whitepaper" on mobile and vice
-// versa
-function changeButtonText(){
-	if($(window).width() < 991){
-		$('.tracer-btn.read-whitepaper').text('Whitepaper')
-	}
-}
-// For use for the blog posts for cutting 
+// For use later for the blog posts for cutting 
 // down the text displayed on the preview
 function truncate(input) {
    if (input.length > 110) {
@@ -58,12 +26,20 @@ function truncate(input) {
    }
    return input;
 };
+$(window).scroll(function(){
+	//Check on the navbar on start
+	navBGScroll();
+});
 $(window).resize(function(){
 
 });
 $(document).ready(function(){
 	setCopyrightYear();
+
+	// Set navbar colour on load if page has been scrolled
 	navBGScroll();
+
+	// Only run parallax on certain pages
 	new universalParallax().init({
 	  speed: 3.0
 	});
