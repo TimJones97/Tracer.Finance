f78caac62cd85e397c4d3cdf1076bac60bf87d9e
Tim Jones <t27.jones@connect.qut.edu.au>
2021-04-04 23:49:27 +1000

    Removed comment from js file

--- media/js/icosahedron.js
+++ media/js/icosahedron.js
@@ -14,8 +14,6 @@ renderer.setSize(600, 600);
 
 // Making the canvas responsive
 function onWindowResize(){
-    console.log('hi');
-
   var screenWidth = $(window).width();
   if (screenWidth <= 440) {
     renderer.setSize(310, 310);
@@ -82,14 +80,6 @@ orbit.minPolarAngle = Math.PI * 0.3; // radians
 orbit.maxPolarAngle = Math.PI * 0.7; // radians
 orbit.minAzimuthAngle = - Math.PI / 2 * 0.6 // radians
 orbit.maxAzimuthAngle = (Math.PI / 2) * 0.6; // radians
-
-var hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-hemiLight.position.set( 0, 300, 0 );
-scene.add( hemiLight );
-
-var dirLight = new THREE.DirectionalLight( 0xffffff );
-dirLight.position.set( 75, 300, -75 );
-scene.add( dirLight );
 
 // sprites
 var txtLoader = new THREE.TextureLoader();
